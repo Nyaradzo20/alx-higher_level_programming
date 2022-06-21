7ae6312c465294477be8a18d1e90e767d402125d
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-06-22 00:14:55 +0200

    added new printing method

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
 print("\"Programming is like building a multilingual puzzle")#can also use masingle quotes instead of double quotes and a slash
+#print('"Programming is like building a multilingual\ puzzle')
