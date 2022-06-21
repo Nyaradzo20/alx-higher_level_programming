a922c543f4c8a2fdbaf587a55893b75220b9ffd1
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-06-22 00:15:53 +0200

    removed backslash

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print("\"Programming is like building a multilingual puzzle")#can also use masingle quotes instead of double quotes and a slash
-#print('"Programming is like building a multilingual\ puzzle')
+#print('"Programming is like building a multilingual puzzle')
