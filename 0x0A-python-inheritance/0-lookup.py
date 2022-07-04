86208a4478d5ef8119313b69e159f99d37a65357
Nyaradzo20 <nchinyati20@gmail.com>
2022-07-04 15:57:57 -0700

    uploading

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to lookup all functions and attributes"""
+
+
+def lookup(obj):
+    """Function to return list of available functions & attributes"""
+    return dir(obj)
