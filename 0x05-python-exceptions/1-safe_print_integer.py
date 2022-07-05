c0727cf440ef0a3f3df430472332254c41ce3601
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-07-05 11:23:39 +0200

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,3 +6,13 @@ def safe_print_integer(value):
         return (True)
     except:
         return (False)
+'''
+def safe_print_integer(value):
+   try:
+       if value == int(value):
+          print("{:d}".format(value))
+          return (True)
+   except:
+      return (False)
+
+'''
