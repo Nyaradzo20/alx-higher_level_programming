a56d30ac40ec2c09dc26f37e36dc510513878945
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-07-20 14:42:17 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,3 +9,9 @@ def read_file(filename=""):
 
     with open(filename, encoding="utf-8") as readFile:
         print(readFile.read(), end='')
+        
+        '''def read_file(filename=""):
+    with open(filename, encoding="utf-8")\ as f:
+        read_data = f.read()
+        print(read_data)'''
+        
